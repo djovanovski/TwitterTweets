425d593edda9e022d895110f177c4f16b0835dc7
djovanovski <djovanovski.dev@gmail.com>
2018-12-19 22:10:12 +0100

    initial commit

---
+++ TwitterTweets/TwitterTweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterTweets
+//
+//  Created by Darko Jovanovski on 12/19/18.
+//  Copyright © 2018 Darko Jovanovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
